c3fd1d64bfd536b30dca15a7c4604707b36b2346
Jothin-kumar <contact@jothin.tech>
2026-05-29 17:54:42 +0530

    init

---
+++ main.js
@@ -0,0 +1,26 @@
+const TabChange=new Event("TabChange");
+window.currentTab=0;
+const tabBtns=document.getElementById("op-chooser").children;
+const tabs=document.getElementById("ops").children;
+function setTab(tab) {
+    tabBtns[window.currentTab].classList.remove("active");
+    tabBtns[tab].classList.add("active");
+    window.currentTab=tab;
+    dispatchEvent(TabChange);
+}
+for (let i=0;i<tabBtns.length;i++) {
+    const btn=tabBtns[i];
+    btn.addEventListener("click",()=>{
+        setTab(i);
+    });
+}
+window.addEventListener("TabChange",()=>{
+    for (let i=0;i<tabs.length;i++) {
+        const tab=tabs[i];
+        if (i==window.currentTab) {
+            tab.classList.add("active");
+        } else {
+            tab.classList.remove("active");
+        }
+    }
+});
